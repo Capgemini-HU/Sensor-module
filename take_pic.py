ccec1f163a9c84f93e4b4a053723b61b88a45560
Cris <crisvdnolle@hotmail.com>
2020-06-26 11:36:39 +0200

    Updated so that the code writes all images into specific folder.

--- take_pic.py
+++ take_pic.py
@@ -1,8 +1,12 @@
 import time
+import os
 from subprocess import call
+
+if not os.path.exists("images"):
+	os.makedirs("images")
 
 # -S is the argument to call "skip frames". 
 # 10 frames are skipped so the camera is able to adjust brightness.
 
 # Time format e.g. "10:14-26-06-2020.jpg"
-call(["fswebcam", "-r 1280x720", "-S 10", "./%H:%M-%d-%m-%Y.jpg"])
+call(["fswebcam", "-r 1280x720", "-S 10", "./images/%H:%M-%d-%m-%Y.jpg"])
